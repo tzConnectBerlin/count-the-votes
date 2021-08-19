cd7e961e7b18f6a3566264560b89609b750e826a
John Newby <john@newby.org>
2021-08-19 17:30:11 +0200

    Initial commit

---
+++ get-tz-profile-list.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl 'https://indexer.tzprofiles.com/v1/graphql' -H 'content-type: application/json' -H 'x-hasura-admin-secret: WJWv4w28yu954DYbN3dFXd0gXsQMX37L' --data-raw '{"query":"query MyQuery {\n  tzprofiles_aggregate(distinct_on: account) {\n    nodes {\n      account\n    }\n  }\n}\n","variables":null,"operationName":"MyQuery"}' |jq ' .data.tzprofiles_aggregate.nodes | map(.account)'
